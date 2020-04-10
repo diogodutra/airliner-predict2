c06772fc77b5aa9669cf98cafc4fedb64ff64efe
Diogo Dutra <diogodutra@gmail.com>
2020-04-09 23:22:16 -0300

    Added dimensions to input

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template
 import pickle
 import numpy as np 
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import MinMaxScaler
 
 app = Flask(__name__)
 
@@ -12,21 +13,28 @@ def home():
 @app.route('/getdelay', methods=['POST','GET'])
 def get_delay():
     if request.method=='POST':
+
         result = request.form
+
         age = float(result['age'])
         leukocytes = float(result['leukocytes'])
         monocytes = float(result['monocytes'])
         platelets = float(result['platelets'])
 
+        leukocytes = leukocytes / 11e3 * 6 - 2
+        monocytes = monocytes / 1e3 * 6 - 2
+        platelets = platelets / 450e3 * 6 - 2
+        # age = round(age / 20)
+
         cat_vector = [[leukocytes, monocytes, platelets, age]]
         
-        # with open('scaler.pkl', 'rb') as f:
-        #     scaler = pickle.load(f)
+        with open('scaler.pkl', 'rb') as f:
+            scaler = pickle.load(f)
         
         with open('logmodel.pkl', 'rb') as f:
             logmodel = pickle.load(f)
 
-        # cat_vector = scaler.transform(cat_vector)
+        cat_vector = scaler.transform(cat_vector)
         prediction = logmodel.predict(cat_vector)
         
         return render_template('result.html', prediction=prediction)
