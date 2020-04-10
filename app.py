a57fbd5578de5cc59be2498c5128ae456cef6024
Diogo Dutra <diogodutra@gmail.com>
2020-04-09 21:10:53 -0300

    Replaced model

--- app.py
+++ app.py
@@ -12,39 +12,43 @@ def home():
 def get_delay():
     if request.method=='POST':
         result=request.form
-        origin = result['origin']
-        dest = result['dest']
-        unique_carrier = result['unique_carrier']
-        day_of_week = result['day_of_week']
-        dep_hour = result['dep_hour']
+        age = result['age']
+        leukocytes = result['leukocytes']
+        monocytes = result['monocytes']
+        platelets = result['platelets']
 
-        pkl_file = open('cat', 'rb')
-        index_dict = pickle.load(pkl_file)
-        cat_vector = np.zeros(len(index_dict))
+        cat_vector = [[age, leukocytes, monocytes, platelets]]
+
+        # pkl_file = open('cat', 'rb')
+        # index_dict = pickle.load(pkl_file)
+        # cat_vector = np.zeros(len(index_dict))
         
-        try:
-            cat_vector[index_dict['DAY_OF_WEEK_'+str(day_of_week)]] = 1
-        except:
-            pass
-        try:
-            cat_vector[index_dict['UNIQUE_CARRIER_'+str(unique_carrier)]] = 1
-        except:
-            pass
-        try:
-            cat_vector[index_dict['ORIGIN_'+str(origin)]] = 1
-        except:
-            pass
-        try:
-            cat_vector[index_dict['DEST_'+str(dest)]] = 1
-        except:
-            pass
-        try:
-            cat_vector[index_dict['DEP_HOUR_'+str(dep_hour)]] = 1
-        except:
-            pass
+        # try:
+        #     cat_vector[index_dict['DAY_OF_WEEK_'+str(day_of_week)]] = 1
+        # except:
+        #     pass
+        # try:
+        #     cat_vector[index_dict['UNIQUE_CARRIER_'+str(unique_carrier)]] = 1
+        # except:
+        #     pass
+        # try:
+        #     cat_vector[index_dict['ORIGIN_'+str(origin)]] = 1
+        # except:
+        #     pass
+        # try:
+        #     cat_vector[index_dict['DEST_'+str(dest)]] = 1
+        # except:
+        #     pass
+        # try:
+        #     cat_vector[index_dict['DEP_HOUR_'+str(dep_hour)]] = 1
+        # except:
+        #     pass
         
-        pkl_file = open('logmodel.pkl', 'rb')
-        logmodel = pickle.load(pkl_file)
+        pkl_scaler = open('scaler.pkl', 'rb')
+        pkl_model = open('logmodel.pkl', 'rb')
+        logmodel = pickle.load(pkl_model)
+
+        cat_vector = pkl_scaler.trasform(cat_vector)
         prediction = logmodel.predict(cat_vector)
         
         return render_template('result.html',prediction=prediction)
