9119d3ba88d3ffb684d7fbc8c03db461ceb0b451
Diogo Dutra <diogodutra@gmail.com>
2020-04-11 22:08:32 -0400

    Replaced model for simple decision tree

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask, request, render_template
 import pickle
 import numpy as np 
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.preprocessing import MinMaxScaler
 
 app = Flask(__name__, static_folder='templates/assets')
+
 
 @app.route('/')
 def home():
@@ -27,21 +29,29 @@ def get_delay():
         str_monocytes = result['monocytes']
         str_platelets = result['platelets']
 
+        # convert to Albert Einstein dataset
         age = float(str_age) # / 20 # round()
         leukocytes = float(str_leukocytes) / 11e3 * 6 - 2
         monocytes = float(str_monocytes) / 1e3 * 6 - 2
         platelets = float(str_platelets) / 450e3 * 6 - 2
 
+        # create input vector for model
         cat_vector = [[leukocytes, monocytes, platelets, age]]
         
-        with open('scaler.pkl', 'rb') as f:
-            scaler = pickle.load(f)
+        # with open('scaler.pkl', 'rb') as f:
+        #     scaler = pickle.load(f)
         
-        with open('logmodel.pkl', 'rb') as f:
-            logmodel = pickle.load(f)
+        # with open('logmodel.pkl', 'rb') as f:
+        #     model = pickle.load(f)
 
-        cat_vector = scaler.transform(cat_vector)
-        prediction = logmodel.predict(cat_vector)
+        # cat_vector = scaler.transform(cat_vector)
+        # prediction = model.predict(cat_vector)
+
+        prediction = [1]
+        if leukocytes < -1.5 : prediction = [0]
+        if leukocytes >   .32: prediction = [0]
+        if platelets  < -2.1 : prediction = [0]
+        if platelets  >  1.8 : prediction = [0]
         
         # return render_template('result.html', prediction=prediction)
         return render_template('triagem-resultado.html',
